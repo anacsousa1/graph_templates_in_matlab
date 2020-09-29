34af8a7b664632cc26c228efbeaf2fbfdade00a7
Ana de Sousa <anacsousa1@gmail.com>
2020-09-29 15:24:34 -0300

    Created new folder and file for line plots.

---
+++ line_plot/line_plot.m
@@ -0,0 +1,8 @@
+
+%% Set colors
+% These colors are colorblind safe and print friendly
+light_green     = [166,219,160]./255;
+dark_green      = [0,136,55]./255;
+light_purple    = [194,165,207]./255;
+dark_purple     = [123,50,148]./255;
+
